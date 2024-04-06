aef83b7e8c88e18561270a7c8fc00b72fd6eaa05
Kelsier1027 <w6886423e@gmail.com>
2024-04-06 17:04:15 +0800

    修正循環引用造成的堆疊溢出錯誤

--- team1FrontEnd.Server/Dtos/CarOrder.cs
+++ team1FrontEnd.Server/Dtos/CarOrder.cs
@@ -1,39 +1,46 @@
-﻿using Microsoft.AspNetCore.Components.Web;
-using team1FrontEnd.Server.Dtos;
+﻿using team1FrontEnd.Server.Dtos;
 
 namespace team1FrontEnd.Server.Models
 {
-	public partial class AttractionTicket : ICartItem
-	{
-		public string Name => this.TicketTitle;
-		public string FileName => $"{this.Attraction.AttractionImages}";
-		int ICartItem.Price => (int)this.Price;
-		
-	}
-	public partial class HotelRoom : ICartItem
-	{
-		public int Price => this.WeekdayPrice;
+    public partial class AttractionTicket : ICartItem
+    {
+        public string Name => this.TicketTitle;
+        public string FileName => $"{this.Attraction.AttractionImages}";
+        int ICartItem.Price => (int)this.Price;
 
-		public string FileName => $"{this.FileName}";
+    }
+    public partial class HotelRoom : ICartItem
+    {
+        public int Price => this.WeekdayPrice;
 
-		private bool WutDay()
-		{
-			DateTime now = DateTime.Now;
-			DayOfWeek dayOfWeek = now.DayOfWeek;
-			if (dayOfWeek == DayOfWeek.Sunday || dayOfWeek == DayOfWeek.Saturday) 
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			};
-		}
+        //public string FileName => $"{this.FileName}";
+        public string FileName
+        {
+            get
+            {
+                // 假设你想返回第一个HotelRoomImage的FileName
+                return this.HotelRoomImages.FirstOrDefault()?.FileName;
+            }
+        }
 
-	}
-	public partial class Package : ICartItem
-	{
-		public string FileName => $"{Image}";
-	}
-	
+        private bool WutDay()
+        {
+            DateTime now = DateTime.Now;
+            DayOfWeek dayOfWeek = now.DayOfWeek;
+            if (dayOfWeek == DayOfWeek.Sunday || dayOfWeek == DayOfWeek.Saturday)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            };
+        }
+
+    }
+    public partial class Package : ICartItem
+    {
+        public string FileName => $"{Image}";
+    }
+
 }
